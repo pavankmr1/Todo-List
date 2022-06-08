75f4c23754ff3487d9d8b4c0c6afd05077b5aa47
Pavankmr1 <pavankmr4501@gmail.com>
2022-06-08 19:25:42 +0530

    Todo List

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let items =["Swimming","Bathing","Breakfast","UdemyCourse"];
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"))
+
+app.get('/', function(req, res) {
+    let today = new Date();
+    
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    let day = today.toLocaleDateString("en-US",options);
+    
+        res.render('list',{
+            kindOfDay: day,
+            newListItems : items
+        });
+   
+    
+});
+
+app.post('/',function(req,res){
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
+
+app.listen(3000,function(){
+    console.log("Server listening on port 3000.")
+})
